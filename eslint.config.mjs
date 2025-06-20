b378f461035cb8e934cde0d6f16343c4bd2701fb
Steve Merschel <steve.merschel@preventicus.com>
2025-06-20 20:02:25 +0200

    FOR-196 Sets up eslint and git ignore
    Configures eslint with recommended typescript rules and ignore patterns.
    Adds git ignore file to avoid commit node_modules and build folders.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,52 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+export default [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.es2021,
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint.plugin,
+        },
+        rules: {
+            "indent": ["error", 2, { "SwitchCase": 2 }],
+            "linebreak-style": ["error", "unix"],
+            "quotes": ["error", "double"],
+            "semi": ["error", "never"],
+            "no-console": "error",
+            "eqeqeq": "error",
+            "space-before-function-paren": ["error", "never"],
+            "object-curly-spacing": ["error", "always"],
+            "@typescript-eslint/no-extraneous-class": "off",
+            "comma-spacing": ["error", { "before": false, "after": true }],
+            "arrow-parens": ["error", "as-needed"],
+            "space-in-parens": ["error", "never"],
+            "space-infix-ops": "error",
+            "space-before-blocks": ["error", "always"],
+            "array-bracket-spacing": ["error", "never"],
+            "computed-property-spacing": ["error", "never"],
+            "no-multi-spaces": "error",
+            "no-trailing-spaces": "error",
+            "no-whitespace-before-property": "error",
+            "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 1, "maxBOF": 0 }],
+            "no-empty": ["error", { "allowEmptyCatch": false }],
+            "padded-blocks": ["error", "never"],
+        },
     },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+    {
+        ignores: ["dist/", "node_modules/", "**/*.spec.js"],
     },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+];
